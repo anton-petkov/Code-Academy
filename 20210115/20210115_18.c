cd025be9fb93fe5df590df406080f6a91c1614f8
anton-petkov <at.petkov@yahoo.com>
2021-01-16 00:54:57 +0200

    Update 20210115_18.c

--- 20210115/20210115_18.c
+++ 20210115/20210115_18.c
@@ -1,61 +1,51 @@
 #include <stdio.h>
-#include <string.h>
-#include <locale.h>
-#include <time.h>
-#include <stdlib.h>
-#include <wchar.h>
-
+void renting()
+{
+    int iKarNum = 3;
+    double dKarPrice = 90.00;
+    int iCamNum = 3;
+    double dCamPrice = 100.00;
+    int rez;
+    int days;
+    printf("Ako zelaete karavana natisnete 1\n");
+    printf("Ako zelaete kemper natisnete 2\n");
+    scanf("%d", &rez);
+    while (rez != 1 && rez != 2)
+    {
+        printf("Molq vuvedete 1 ili 2.\n");
+        scanf("%d", &rez);
+    }
+    printf("Molq vuvedete za kolko dni ]e naemete.\n");
+    scanf("%d", &days);
+    if (rez == 1)
+    {
+        printf("Vie iskate karavana koqto struva: %.2f lv.\n", dKarPrice * days);
+    }
+    if (rez == 2)
+    {
+        printf("Vie iskate kemper koqto struva: %.2f lv.\n", dCamPrice * days);
+    }
+}
 int main()
 {
+    int rez;
+    renting();
 
-    float fTomatoPrice = 4.5f;
-    float fTomatoWeight;
-    float fFlourPrice = 1.8f;
-    float fFlourWeight;
-    float fCandyPrice = 1.5f;
-    float fCandyWeight;
-    float fYogurtPrice = 0.5f;
-    float fYogurtNum;
-    float fLollipopPrice = 0.15f;
-    float fLollipopNum;
-    float fIceCreamPrice = 0.6f;
-    float fIceCreamtNum;
-
-    printf("Welcome to our shop! Today we offer : tomatoes, flour, candy, yogurt, lollipos, Icecream.\n");
-    printf("What quantity of tomatoes would you need? If not please type \"0\" ");
-    scanf("%f", &fTomatoWeight);
-
-    printf("What quantity of flour would you need?  If not please type \"0\" ");
-    scanf("%f", &fFlourWeight);
-
-    printf("What quantity of candy would you need? If not please type \"0\" ");
-    scanf("%f", &fCandyWeight);
-
-    printf("What quantity of yogurt would you need? If not please type \"0\" ");
-    scanf("%f", &fYogurtNum);
-
-    printf("What quantity of lollipos would you need? If not please type \"0\" ");
-    scanf("%f", &fLollipopNum);
-
-    printf("What quantity of icecream would you need? If not please type \"0\" ");
-    scanf("%f", &fIceCreamtNum);
-
-    float fTomatoTotal = fTomatoWeight * fTomatoPrice;
-    float fFlourTotal = fFlourWeight * fFlourPrice;
-    float fCandyTotal = fCandyPrice * fCandyWeight;
-    float fYogurtTotal = fYogurtNum * fYogurtPrice;
-    float fLollipopTotal = fLollipopNum * fLollipopPrice;
-    float fIceCreamTotal = fIceCreamPrice * fIceCreamtNum;
-
-    printf("Order:\n");
-    printf("Tomatoes: %.2f lv.\n", fTomatoTotal);
-    printf("Flour: %.2f lv.\n", fFlourTotal);
-    printf("Candy: %.2f lv.\n", fCandyTotal);
-    printf("Yogurt: %.2f lv.\n", fYogurtTotal);
-    printf("Lollipops: %.2f lv.\n", fLollipopTotal);
-    printf("Icecream: %.2f lv.\n", fIceCreamTotal);
-    float fTotal = fIceCreamTotal + fLollipopTotal + fTomatoTotal + fYogurtTotal + fFlourTotal + fCandyTotal;
-    printf("Total: %.2f lv.\n", fTotal);
-
+    printf("Ako zelaete ne]o drugo natisnete 1\n");
+    printf("Ako iskate da zawyshite porychkata natisnete 0\n");
+    scanf("%d", &rez);
+    while (rez != 1 && rez != 0)
+    {
+        printf("Molq vuvedete 1 ili 0.\n");
+        scanf("%d", &rez);
+    }
+    if (rez == 1)
+    {
+        renting();
+    }
+    if (rez == 0)
+    {
+        printf("Blagodarim za wa[ata porychka!");
+    }
     return 0;
 }
